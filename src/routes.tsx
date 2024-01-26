4747990b3c6456edbea87ac5c8bf1149d9157701
Marcelo <marcelolavratti@gmail.com.br>
2024-01-26 18:35:35 -0300

    first commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 
 import MainPage from './pages/Main'
+import Burgers from './pages/Main/Burgers/burgers'
+import Pizzas from './pages/Main/Pizzas/pizzas'
+import { Drinks } from './pages/Main/Drinks/drinks'
+import { IceCreams } from './pages/Main/IceCreams/iceCreams'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<MainPage />} />
+      <Route path='/' element={<MainPage />}>
+        <Route path='/' element={<Burgers />} />
+        <Route path='pizzas' element={<Pizzas />} />
+        <Route path='drinks' element={<Drinks />} />
+        <Route path='ice-creams' element={<IceCreams />} />
+      </Route>
     </Routes>
   )
 }
